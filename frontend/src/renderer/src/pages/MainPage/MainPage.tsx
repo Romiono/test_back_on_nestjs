d13c6f056e527e35fcf3c47b6a876ced5eb0b8f8
Romiono <roma.serebrennikov.00@mail.ru>
2025-03-17 21:22:16 +0300

    fix last commit

--- frontend/src/renderer/src/pages/MainPage/MainPage.tsx
+++ frontend/src/renderer/src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,56 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
+import classes from './MainPage.module.scss';
+import { Button } from '@mui/material';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { Dayjs } from 'dayjs';
+
+interface Date {
+  formatedDate: string;
+  date: Dayjs;
+}
 
 const MainPage = (): ReactElement => {
-  return (
-    <div>
+const [date, setDate] = useState<Date | null>(null);
 
+  return (
+    <div className={classes.main}>
+      <div className={classes.main__content}>
+        <div className={classes.main__content__recordByDate}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Выберите дату"
+              value={null}
+              onChange={(value: Dayjs | null) => {
+                if(value === null) return;
+                const formatedDate = value.format('YYYY-MM-DD');
+                setDate({ date: value, formatedDate });
+                console.log(date);
+              }}
+            />
+          </LocalizationProvider>
+          <Button variant={'contained'} sx={{ height: '100%' }}>
+            Получить отчет
+          </Button>
+        </div>
+        <div className={classes.main__content__recordByDate}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Выберите дату"
+              value={null}
+              onChange={(value: Dayjs | null) => {
+                if(value === null) return;
+                const formatedDate = value.format('YYYY-MM-DD');
+                setDate({ date: value, formatedDate });
+                console.log(date);
+              }}
+            />
+          </LocalizationProvider>
+          <Button variant={'contained'} sx={{ height: '100%' }}>
+            Получить отчет
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
